42cdf8c17a823381bf0d6511a147f90ed0985aef
JosueGit20 <JosueGit20@github.com>
2020-02-26 22:58:56 -0600

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,11 @@
+import os 
+contador = 0
+for h in range(1, 255, 1):
+    if os.system("ping 200.33.171."+str(h)) == 0:
+        contador +=1
+        print(" ->SÍ está activa")
+    else:
+        print(" -> NO está activa")
+
+print("Total activas: " + contador + "\n Total inactivas: " + (255-contador))
+
